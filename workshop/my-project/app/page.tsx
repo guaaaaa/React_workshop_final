08e7619e4b9675bee003e64b5b42adf5bc1d4f36
Aaron Gu <gu20041006@gmail.com>
2024-10-23 16:23:49 -0400

    Exchange commit

--- workshop/my-project/app/page.tsx
+++ workshop/my-project/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
         <h1 className="text-5xl p-2 m-4">Home Page</h1>
         <Link className="border-2 p-2 m-4" href="/counter">Counter</Link>
         <Link className="border-2 p-2 m-4" href="/exchange">Exchange App</Link>
-        <Link className="border-2 p-2 m-4" href="/password-generator">Password Generator</Link>
       </div>
     </div>
   );
