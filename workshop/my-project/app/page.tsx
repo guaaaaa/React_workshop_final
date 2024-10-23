3aee1196aa1389f3204952a3f861b141dec7f839
Aaron Gu <gu20041006@gmail.com>
2024-10-23 16:22:13 -0400

    Counter commit

--- workshop/my-project/app/page.tsx
+++ workshop/my-project/app/page.tsx
@@ -1,13 +1,25 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="h-screen w-screen flex bg-pink-400 justify-center items-center">
-      <div className="flex flex-col text-center">
-        <h1 className="text-5xl p-2 m-4">Home Page</h1>
-        <Link className="border-2 p-2 m-4" href="/counter">Counter</Link>
-        <Link className="border-2 p-2 m-4" href="/exchange">Exchange App</Link>
-        <Link className="border-2 p-2 m-4" href="/password-generator">Password Generator</Link>
+    <div className="h-screen w-screen flex bg-pink-400 justify-center items-center relative">
+      <div>
+        <h1 className="text-5xl flex justify-center">This is a Counter!!!</h1>
+        <h1 className="flex justify-center text-9xl m-6">{count}</h1>
+
+        <div className="flex justify-center">
+          <button className="border-2 p-2 m-2" onClick={increase}>Increase</button>
+          <button className="border-2 p-2 m-2" onClick={() => setCount(count - 1)}>Decrease</button>
+          <button className="border-2 p-2 m-2" onClick={() => setCount(0)}>Reset</button>
+        </div>
       </div>
     </div>
   );
